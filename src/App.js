0fc447141fd17fa9dd56bf841aae8594f0eb8da5
amuybar <amuyunzubq@gmail.com>
2024-08-30 15:18:05 +0300

    UseuseEffect(() => {     window.addEventListener('resize', () => setWindowWidth(window.innerWidth));     return () => window.removeEventListener('resize', () => setWindowWidth(window.innerWidth));   }, []);

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+
+export default function App() {
+  const [resourceType, setResourceType] = useState('posts');
+  const [data, setData] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+    return () => window.removeEventListener('resize', () => setWindowWidth(window.innerWidth));
+  }, []);
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  },[resourceType])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Window width is: { windowWidth}</h1>
+      <div>
+        <button onClick={()=>setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>users</button>
+        <button onClick={()=>setResourceType('comments')}>comments</button>
+      </div>
+      {
+        data.length > 0 &&
+        <ul>
+          {data.map((item) => (
+            <pre >{ JSON.stringify(item)}</pre>
+          ))}
+        </ul>
+      }
+    </>
+  )
 }
-
-export default App;
